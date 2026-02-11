85ec577ef1fbb9a4253884796e75569bd91c4db5
KiraDamnatio22 <damnatiokira22@gmail.com>
2026-02-11 09:06:24 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from ui.main_window import MainWindow
+
+if __name__ == "__main__":
+    app = MainWindow()
+    app.mainloop()
+    
+
+# from ui.main_window import MainWindow
+# import cProfile
+
+# if __name__ == "__main__": 
+#     app = MainWindow()
+#     cProfile.run("app.mainloop()")
+
+
+# from ui.db_window import DBWindow
+
+# if __name__ == "__main__":
+    # app = DBWindow()
+    # app.mainloop()
